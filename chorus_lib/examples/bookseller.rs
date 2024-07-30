066d130e940be48c7345719565d9fd258a596b3f
Mako Bates <33739499+ShapeOfMatter@users.noreply.github.com>
2024-07-30 17:24:02 -0400

    WIP: add tests, doesnt' seem to work...

--- chorus_lib/examples/bookseller.rs
+++ chorus_lib/examples/bookseller.rs
@@ -3,7 +3,10 @@ extern crate chorus_lib;
 use std::io;
 use std::thread;
 
-use chorus_lib::transport::local::LocalTransportChannelBuilder;
+use chorus_lib::{
+    core::{Located},
+    transport::local::{LocalTransportChannelBuilder},
+};
 use chrono::NaiveDate;
 
 use chorus_lib::core::{ChoreoOp, Choreography, ChoreographyLocation, LocationSet, Projector};
@@ -25,17 +28,14 @@ struct Seller;
 #[derive(ChoreographyLocation)]
 struct Buyer;
 
-struct BooksellerChoreography;
-impl Choreography for BooksellerChoreography {
+struct BooksellerChoreography{
+    title: Located<String, Buyer>,
+    budget: Located<i32, Buyer>,
+}
+impl Choreography<bool> for BooksellerChoreography {
     type L = LocationSet!(Seller, Buyer);
-    fn run(self, op: &impl ChoreoOp<Self::L>) {
-        let title_at_buyer = op.locally(Buyer, |_| {
-            println!("Enter the title of the book to buy (TAPL or HoTT)");
-            let mut title = String::new();
-            io::stdin().read_line(&mut title).unwrap();
-            title
-        });
-        let title_at_seller = op.comm(Buyer, Seller, &title_at_buyer);
+    fn run(self, op: &impl ChoreoOp<Self::L>) -> bool {
+        let title_at_seller = op.comm(Buyer, Seller, &self.title);
         let price_at_seller = op.locally(Seller, |un| {
             let title = un.unwrap(&title_at_seller);
             if let Some((price, _)) = get_book(&title) {
@@ -47,7 +47,7 @@ impl Choreography for BooksellerChoreography {
         let decision_at_buyer = op.locally(Buyer, |un| {
             if let Some(price) = un.unwrap(&price_at_buyer) {
                 println!("Price is {}", price);
-                return *price < BUDGET;
+                return *price < *un.unwrap(&self.budget);
             }
             println!("The book does not exist");
             return false;
@@ -69,10 +69,15 @@ impl Choreography for BooksellerChoreography {
                 println!("The buyer cannot buy the book");
             });
         }
+        decision
     }
 }
 
 fn main() {
+    println!("Enter the title of the book to buy (TAPL or HoTT)");
+    let mut title = String::new();
+    io::stdin().read_line(&mut title).unwrap();
+
     let transport_channel = LocalTransportChannelBuilder::new()
         .with(Seller)
         .with(Buyer)
@@ -85,12 +90,55 @@ fn main() {
 
     let mut handles: Vec<thread::JoinHandle<()>> = Vec::new();
     handles.push(thread::spawn(move || {
-        seller_projector.epp_and_run(BooksellerChoreography);
+        seller_projector.epp_and_run(BooksellerChoreography{
+            title: seller_projector.remote(Buyer),
+            budget: seller_projector.remote(Buyer),
+        });
     }));
     handles.push(thread::spawn(move || {
-        buyer_projector.epp_and_run(BooksellerChoreography);
+        buyer_projector.epp_and_run(BooksellerChoreography{
+            title: buyer_projector.local(title),
+            budget: buyer_projector.local(BUDGET),
+        });
     }));
     for h in handles {
         h.join().unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn distributed_tapl() {
+        let title = String::new("TAPL");
+
+        let transport_channel = LocalTransportChannelBuilder::new()
+            .with(Seller)
+            .with(Buyer)
+            .build();
+        let transport_seller = LocalTransport::new(Seller, transport_channel.clone());
+        let transport_buyer = LocalTransport::new(Buyer, transport_channel.clone());
+
+        let seller_projector = Projector::new(Seller, transport_seller);
+        let buyer_projector = Projector::new(Buyer, transport_buyer);
+
+        let mut handles: Vec<thread::JoinHandle<()>> = Vec::new();
+        handles.push(thread::spawn(move || {
+            seller_projector.epp_and_run(BooksellerChoreography{
+                title: seller_projector.remote(Buyer),
+                budget: seller_projector.remote(Buyer),
+            });
+        }));
+        handles.push(thread::spawn(move || {
+            buyer_projector.epp_and_run(BooksellerChoreography{
+                title: buyer_projector.local(title),
+                budget: buyer_projector.local(BUDGET),
+            });
+        }));
+        for h in handles {
+            assert!(h.join().unwrap());
+        }
     }
 }
