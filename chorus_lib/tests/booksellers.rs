c7ff7ea64e77e9a58e2ec26b99ca2e76908b520a
Mako Bates <33739499+ShapeOfMatter@users.noreply.github.com>
2024-10-18 16:49:14 -0400

    it runs

--- chorus_lib/tests/booksellers.rs
+++ chorus_lib/tests/booksellers.rs
@@ -1,6 +1,7 @@
 extern crate chorus_lib;
 
 use std::collections::HashMap;
+use std::marker::PhantomData;
 use std::sync::Arc;
 use std::thread;
 
@@ -24,25 +25,87 @@ type Title = String;
 
 type Inventory = HashMap<Title, (Money, NaiveDate)>;
 
-type Decider = fn(Located<Option<Money>, Buyer1>) -> Box<dyn Choreography<Located<bool, Buyer1>, L = LocationSet!(Buyer1, Buyer2)>>;
 
-fn unilateral(budget: Located<Money, Buyer1>) -> Decider {
-    |price| {
-        struct Choreo{
-            price: Located<Money, Buyer1>,
-            budget: Located<Money, Buyer1>,
+trait Decider {
+    type Budgets;
+    fn new(price: Located<Option<Money>, Buyer1>, budgets: Self::Budgets) -> Self;
+}
+
+struct Booksellers<D: Choreography<Located<bool, Buyer1>> + Decider<Budgets=Budgets>, Budgets> {
+    inventory: Located<Inventory, Seller>,
+    title: Located<Title, Buyer1>,
+    budgets: Budgets,
+    _marker: PhantomData<D>,
+}
+
+impl<D: Choreography<Located<bool, Buyer1>, L = LocationSet!(Buyer1, Buyer2)> + Decider<Budgets=Budgets>, Budgets>
+     Choreography<Option<NaiveDate>> for Booksellers<D, Budgets>
+{
+    type L = LocationSet!(Buyer1, Buyer2, Seller);
+    fn run(self, op: &impl ChoreoOp<Self::L>) -> Option<NaiveDate> {
+        let title_at_seller = op.comm(Buyer1, Seller, &self.title);
+        let price_at_seller = op.locally(Seller, |un| {
+            let inventory = un.unwrap(&self.inventory);
+            let title = un.unwrap(&title_at_seller);
+            inventory.get(title).map(|(price, _)|{*price})        });
+        let price_at_buyer1 = op.comm(Seller, Buyer1, &price_at_seller);
+        let decider = D::new(price_at_buyer1, self.budgets);
+        let decision_at_buyer1 = op.call(decider);
+
+        struct GetDeliveryDateChoreography {
+            inventory: Located<Inventory, Seller>,
+            title_at_seller: Located<Title, Seller>,
+            decision_at_buyer1: Located<bool, Buyer1>,
         }
-        impl Choreography<Located<bool, Buyer1>> for Choreo {
-            type L = LocationSet!(Buyer1, Buyer2);
-            fn run(self, op: &impl ChoreoOp<Self::L>) -> Located<bool, Buyer1> {
-                op.locally(Buyer1, |un| {
-                    un.unwrap(&self.price) <= un.unwrap(&self.budget)
-                })
+        impl Choreography<Located<Option<NaiveDate>, Buyer1>> for GetDeliveryDateChoreography {
+            type L = LocationSet!(Buyer1, Seller);
+            fn run(self, op: &impl ChoreoOp<Self::L>) -> Located<Option<NaiveDate>, Buyer1> {
+                let decision = op.broadcast(Buyer1, self.decision_at_buyer1);
+                if decision {
+                    let delivery_date_at_seller = op.locally(Seller, |un| {
+                        let title = un.unwrap(&self.title_at_seller);
+                        let inventory = un.unwrap(&self.inventory);
+                        inventory.get(title).map(|(_, delivery_date)| {*delivery_date})
+                    });
+                    let delivery_date_at_buyer1 = op.comm(Seller, Buyer1, &delivery_date_at_seller);
+                    return delivery_date_at_buyer1;
+                } else {
+                    return op.locally(Buyer1, |_| None);
+                }
             }
         }
-        Box::new(Choreo{price, budget})
+
+        return op.broadcast(Buyer1,
+                            op.enclave(GetDeliveryDateChoreography {
+                                        inventory: self.inventory.clone(),
+                                        title_at_seller: title_at_seller.clone(),
+                                        decision_at_buyer1,
+                                       }).flatten());
     }
 }
+
+struct Unilateral {
+    price: Located<Option<Money>, Buyer1>,
+    budget: Located<Money, Buyer1>,
+}
+impl Decider for Unilateral {
+    type Budgets = Located<Money, Buyer1>;
+    fn new(price: Located<Option<Money>, Buyer1>, budgets: Located<Money, Buyer1>) -> Self{
+        return Self{price: price, budget: budgets}
+    }
+}
+impl Choreography<Located<bool, Buyer1>> for Unilateral {
+    type L = LocationSet!(Buyer1, Buyer2);
+    fn run(self, op: &impl ChoreoOp<Self::L>) -> Located<bool, Buyer1> {
+        op.locally(Buyer1, |un| {
+            match un.unwrap(&self.price) {
+                Some(price) => price <= un.unwrap(&self.budget),
+                None => false
+            }
+        })
+    }
+}
+
 
 //fn collaborative(budget1: Located<Money, Buyer1>, budget2: Located<Money, Buyer2>) -> Decider {
 //    |price| {
@@ -65,95 +128,12 @@ fn unilateral(budget: Located<Money, Buyer1>) -> Decider {
 //}
 
 
-struct BooksellerChoreography{
-    decider: Decider,
-    inventory: Located<Inventory, Seller>,
-    title: Located<Title, Buyer1>,
-}
 
-impl Choreography<Option<NaiveDate>> for BooksellerChoreography
-{
-    type L = LocationSet!(Buyer1, Buyer2, Seller);
-    fn run(self, op: &impl ChoreoOp<Self::L>) -> Option<NaiveDate> {
-        let title_at_seller = op.comm(Buyer1, Seller, &self.title);
-        let price_at_seller = op.locally(Seller, |un| {
-            let inventory = un.unwrap(&self.inventory);
-            let title = un.unwrap(&title_at_seller);
-            match inventory.get(title) {
-                Some((price, _)) => Some(price),
-                None => None,
-            }
-        });
-        let price_at_buyer1 = op.comm(Seller, Buyer1, &price_at_seller);
-        let decision_at_buyer1 = op.enclave(&self.decider(price_at_buyer1));
-
-        struct GetDeliveryDateChoreography {
-            inventory: Located<Inventory, Seller>,
-            title_at_seller: Located<Title, Seller>,
-            decision_at_buyer1: Located<bool, Buyer1>,
-        }
-        impl Choreography<Located<Option<NaiveDate>, Buyer1>> for GetDeliveryDateChoreography {
-            type L = LocationSet!(Buyer1, Seller);
-            fn run(self, op: &impl ChoreoOp<Self::L>) -> Located<Option<NaiveDate>, Buyer1> {
-                let decision = op.broadcast(Buyer1, self.decision_at_buyer1);
-                if decision {
-                    let delivery_date_at_seller = op.locally(Seller, |un| {
-                        let title = un.unwrap(&self.title_at_seller);
-                        let inventory = un.unwrap(&self.inventory);
-                        match inventory.get(title) {
-                            Some((_, delivery_date)) => Some(delivery_date),
-                            None => None,
-                        }
-                    });
-                    let delivery_date_at_buyer1 = op.comm(Seller, Buyer1, &delivery_date_at_seller);
-                    return delivery_date_at_buyer1;
-                } else {
-                    return op.locally(Buyer1, |_| None);
-                }
-            }
-        }
-
-        return op.enclave(GetDeliveryDateChoreography {
-            inventory: self.inventory.clone(),
-            title_at_seller: title_at_seller.clone(),
-            decision_at_buyer1,
-        });
-    }
-}
-
-/*fn locate<A, P: ChoreographyLocation>(a: A, p: P) -> Located<A, P> {
-    struct Dummy<A1, P1: ChoreographyLocation> {
-        a: RC<A1>,
-        p: P1
-    }
-    impl<A2, P2> Choreography<Located<A2, P2>> for Dummy<A2, P2>
-    where P2: ChoreographyLocation {
-        type L = LocationSet!(P2);
-        fn run(self, op: &impl ChoreoOp<Self::L>) -> Located<A2, P2> {
-            op.locally(self.p, |_| self.a)
-        }
-    }
-    Runner::new().run(Dummy{a, p});
-}*/
 
 fn run_test(inventory: Inventory, title: Title, budget1: Money, budget2: Option<Money>, answer: Option<NaiveDate>) {
-    let central_runner = Runner::new();
-    let decider: Decider = match budget2 {
-        Some(b2) => panic!(),
-        None => unilateral(central_runner.local(budget1)),
-    };
-
-    let central_result = central_runner.run(BooksellerChoreography{
-        decider,
-        inventory: central_runner.local(inventory),
-        title: central_runner.local(title),
-    });
-    assert_eq!(central_result, answer);
-
     let transport_channel = LocalTransportChannelBuilder::new()
         .with(Seller).with(Buyer1).with(Buyer2)
         .build();
-
     let seller_projector = Arc::new(Projector::new(
         Seller,
         LocalTransport::new(Seller, transport_channel.clone()),
@@ -168,38 +148,54 @@ fn run_test(inventory: Inventory, title: Title, budget1: Money, budget2: Option<
     ));
     let mut handles = Vec::new();
 
-    if let Some(budget2) = budget2 {
-
+    if let Some(_) = budget2 {
+        panic!() //TODO
     } else {
         {
+            let central_runner = Runner::new();
+            let choreo : Booksellers<Unilateral, Located<Money, Buyer1>> = Booksellers{
+                inventory: central_runner.local(inventory.clone()),
+                title: central_runner.local(title.clone()),
+                budgets: central_runner.local(budget1),
+                _marker: PhantomData,
+            };
+            let central_result = central_runner.run(choreo);
+            assert_eq!(central_result, answer);
+        }
+        {
             let seller_projector = seller_projector.clone();
-            let inventory = inventory.clone();
             handles.push(thread::spawn(move || {
-                seller_projector.epp_and_run(BooksellerChoreography {
-                    decider,
-                    inventory: seller_projector.local(inventory),
+                let choreo : Booksellers<Unilateral, Located<Money, Buyer1>> = Booksellers{
+                    inventory: seller_projector.local(inventory.clone()),
                     title: seller_projector.remote(Buyer1),
-                });
+                    budgets: seller_projector.remote(Buyer1),
+                    _marker: PhantomData,
+                };
+                seller_projector.epp_and_run(choreo)
             }));
         }
         {
             let buyer1_projector = buyer1_projector.clone();
             handles.push(thread::spawn(move || {
-                buyer1_projector.epp_and_run(BooksellerChoreography {
-                    decider,
+                let choreo : Booksellers<Unilateral, Located<Money, Buyer1>> = Booksellers{
                     inventory: buyer1_projector.remote(Seller),
-                    title: buyer1_projector.local("HoTT".to_string()),
-                });
+                    title: buyer1_projector.local(title).clone(),
+                    budgets: buyer1_projector.local(budget1),
+                    _marker: PhantomData,
+                };
+                buyer1_projector.epp_and_run(choreo)
             }));
         }
         {
             let buyer2_projector = buyer2_projector.clone();
             handles.push(thread::spawn(move || {
-                buyer2_projector.epp_and_run(BooksellerChoreography {
-                    decider,
+                let choreo : Booksellers<Unilateral, Located<Money, Buyer1>> = Booksellers{
                     inventory: buyer2_projector.remote(Seller),
                     title: buyer2_projector.remote(Buyer1),
-                });
+                    budgets: buyer2_projector.remote(Buyer1),
+                    _marker: PhantomData,
+                };
+                buyer2_projector.epp_and_run(choreo)
             }));
         }
     }
